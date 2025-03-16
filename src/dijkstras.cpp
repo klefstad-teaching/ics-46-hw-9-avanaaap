6d94408fda103db60ecb03f1a1e8bb994b09b1b4
avanaaap <pavanau@uci.edu>
2025-03-16 05:37:50 +0000

    printing again:

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -53,18 +53,13 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 
 void print_path(const vector<int>& path, int total) {
-    if (path.empty()) {
-        cout << "No path found." << endl;
-        return;
-    }
-
-    cout << "Total cost is " << total << "\n";
-
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
         if (i < path.size() - 1) {
             cout << " ";  
         }
     }
-    cout << "\n";  
+
+    cout << " \n";  
+    cout << "Total cost is " << total << "\n";
 }
