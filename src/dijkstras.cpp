0b68c066e097525e99b28e72719dc5fffb77f363
avanaaap <pavanau@uci.edu>
2025-03-16 05:47:38 +0000

    removed extra space 2

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -52,6 +52,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 
+
 void print_path(const vector<int>& path, int total) {
     for (size_t i = 0; i < path.size(); ++i) {
         cout << path[i];
@@ -60,6 +61,5 @@ void print_path(const vector<int>& path, int total) {
         }
     }
 
-    cout <<"\n";  
-    cout <<"Total cost is " << total << "\n";
+    cout << "\nTotal cost is " << total << "\n";
 }
